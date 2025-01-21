8102755026dace6185d7f5dccfac9c8429bcab61
ASEDOV89 <a.sedov@x5.ru>
2025-01-21 19:06:36 +0300

    init

---
+++ blog/views.py
@@ -0,0 +1,46 @@
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # По умолчанию 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    form_class = PostForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
